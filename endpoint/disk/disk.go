41eef88f9ecda2f07136fb35508ef43757bd5165
Jeremy Volkman <volkman@google.com>
2017-06-20 13:02:17 -0700

    Adds a simple disk endpoint.
    The endpoint writes reports to a path on the filesystem and cleans up
    old reports after a configured amount of time.
    
    Change-Id: I0109ec6da3eb6f33fdb7dbb21dc8e202c2e3539b

---
+++ endpoint/disk/disk.go
@@ -0,0 +1,140 @@
+package disk
+
+import (
+	"encoding/json"
+	"github.com/golang/glog"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+	"ubbagent/clock"
+	"ubbagent/endpoint"
+	"ubbagent/metrics"
+)
+
+const (
+	fileMode        = 0755
+	directoryMode   = 0644
+	cleanupInterval = 1 * time.Minute
+	reportPrefix    = "report_"
+	reportSuffix    = ".json"
+)
+
+type DiskEndpoint struct {
+	name       string
+	path       string
+	expiration time.Duration
+	quit       chan bool
+	closeOnce  sync.Once
+	clock      clock.Clock
+}
+
+type diskReport struct {
+	name  string
+	batch metrics.MetricBatch
+}
+
+// NewDiskEndpoint creates a new DiskEndpoint and starts a goroutine that cleans up expired reports
+// on disk.
+func NewDiskEndpoint(name, path string, expiration time.Duration) *DiskEndpoint {
+	return newDiskEndpoint(name, path, expiration, clock.NewRealClock())
+}
+
+func newDiskEndpoint(name, path string, expiration time.Duration, clock clock.Clock) *DiskEndpoint {
+	ep := &DiskEndpoint{
+		name:       name,
+		path:       path,
+		expiration: expiration,
+		clock:      clock,
+		quit:       make(chan bool, 1),
+	}
+	go ep.run()
+	return ep
+}
+
+func (ep *DiskEndpoint) Name() string {
+	return ep.name
+}
+
+func (ep *DiskEndpoint) Send(report endpoint.EndpointReport) error {
+	r := report.(diskReport)
+
+	jsontext, err := json.Marshal(r.batch)
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(ep.path, directoryMode); err != nil {
+		return err
+	}
+	file := path.Join(ep.path, r.name)
+
+	if err := ioutil.WriteFile(file, jsontext, fileMode); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ep *DiskEndpoint) BuildReport(mb metrics.MetricBatch) (endpoint.EndpointReport, error) {
+	return diskReport{
+		name:  reportName(ep.clock.Now()),
+		batch: mb,
+	}, nil
+}
+
+func (*DiskEndpoint) EmptyReport() endpoint.EndpointReport {
+	return diskReport{}
+}
+
+func (ep *DiskEndpoint) Close() error {
+	ep.closeOnce.Do(func() {
+		ep.quit <- true
+	})
+	return nil
+}
+
+func (ep *DiskEndpoint) run() {
+	for {
+		t := ep.clock.NewTimer(cleanupInterval)
+		select {
+		case <-t.GetC():
+			ep.cleanup()
+		case <-ep.quit:
+			return
+		}
+		t.Stop()
+	}
+}
+
+func (ep *DiskEndpoint) cleanup() {
+	// compute time before which files are expired.
+	cutoff := ep.clock.Now().Add(-ep.expiration)
+	files, _ := ioutil.ReadDir(ep.path)
+	for _, f := range files {
+		if isExpired(f.Name(), cutoff) {
+			if err := os.Remove(filepath.Join(ep.path, f.Name())); err != nil {
+				glog.Warningf("error removing expired disk report: %v", f)
+			}
+		}
+	}
+}
+
+func reportName(reportTime time.Time) string {
+	return reportPrefix + reportTime.UTC().Format(time.RFC3339) + reportSuffix
+}
+
+func isExpired(name string, cutoff time.Time) bool {
+	if !strings.HasPrefix(name, reportPrefix) {
+		return false
+	}
+	if !strings.HasSuffix(name, reportSuffix) {
+		return false
+	}
+	t, err := time.Parse(time.RFC3339, name[len(reportPrefix):len(name)-len(reportSuffix)])
+	if err != nil {
+		return false
+	}
+	return t.Before(cutoff)
+}
---
+++ endpoint/disk/disk_test.go
@@ -0,0 +1,107 @@
+package disk
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+	"ubbagent/clock"
+	"ubbagent/metrics"
+)
+
+func TestDiskEndpoint(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "disk_endpoint_test")
+	if err != nil {
+		t.Fatalf("Unable to create temp directory: %+v", err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	mc := clock.NewMockClock()
+	mc.SetNow(parseTime("2017-06-19T12:00:00Z"))
+	ep := newDiskEndpoint("disk", tmpdir, 10*time.Minute, mc)
+
+	// Make sure we start with an empty dir
+	if files, err := ioutil.ReadDir(tmpdir); err != nil {
+		t.Fatalf("error listing output directory: %+v", err)
+	} else if len(files) != 0 {
+		t.Fatalf("output directory contains %v files, expected 0", len(files))
+	}
+
+	// Test a single report write
+	report1, err := ep.BuildReport(metrics.MetricBatch{
+		metrics.MetricReport{
+			Name:      "int-metric1",
+			StartTime: time.Unix(0, 0),
+			EndTime:   time.Unix(1, 0),
+			Value: metrics.MetricValue{
+				IntValue: 10,
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("error building report: %+v", err)
+	}
+	if err := ep.Send(report1); err != nil {
+		t.Fatalf("error sending report: %+v", err)
+	}
+	if err := waitForReportCount(tmpdir, 1); err != nil {
+		t.Fatalf("error waiting for 1 file in output path: %+v", err)
+	}
+
+	mc.SetNow(parseTime("2017-06-19T12:05:00Z"))
+
+	// Test a second report write
+	report2, err := ep.BuildReport(metrics.MetricBatch{
+		metrics.MetricReport{
+			Name:      "int-metric1",
+			StartTime: time.Unix(2, 0),
+			EndTime:   time.Unix(3, 0),
+			Value: metrics.MetricValue{
+				IntValue: 10,
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("error building report: %+v", err)
+	}
+	if err := ep.Send(report2); err != nil {
+		t.Fatalf("error sending report: %+v", err)
+	}
+	if err := waitForReportCount(tmpdir, 2); err != nil {
+		t.Fatalf("error waiting for 2 files in output path: %+v", err)
+	}
+
+	// Test that the first report is removed after expiration (10 minutes in this test)
+	mc.SetNow(parseTime("2017-06-19T12:11:00Z"))
+	if err := waitForReportCount(tmpdir, 1); err != nil {
+		t.Fatalf("error waiting for 1 files in output path: %+v", err)
+	}
+}
+
+func parseTime(ts string) time.Time {
+	t, err := time.Parse(time.RFC3339, ts)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func waitForReportCount(dir string, count int) error {
+	tmr := time.NewTimer(5 * time.Second)
+	tck := time.NewTicker(5 * time.Millisecond)
+	defer tmr.Stop()
+	defer tck.Stop()
+	for {
+		select {
+		case <-tck.C:
+			if files, err := ioutil.ReadDir(dir); err != nil {
+				return err
+			} else if len(files) == count {
+				return nil
+			}
+		case <-tmr.C:
+			return errors.New("timeout")
+		}
+	}
+}
