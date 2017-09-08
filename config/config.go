71dbafc931d8203cee91aad17f8de03abcbc581a
Jeremy Volkman <volkman@google.com>
2017-09-08 11:21:57 -0700

    Adds an encodedServiceAccountKey parameter which takes a base64-encoded key.
    encodedServiceAccountKey can be provided in place of serviceAccountKey.
    Exactly one of the values must be provided.
    
    Additionally, fixed some bad logic in existing unit tests which was hiding
    an existing validation bug.

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@
 package config
 
 import (
-	"encoding/json"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -23,6 +23,7 @@ import (
 	"strings"
 	"sync"
 
+	"encoding/json"
 	"github.com/ghodss/yaml"
 )
 
@@ -30,6 +31,63 @@ const (
 	IntType    = "int"
 	DoubleType = "double"
 )
+
+// LiteralServiceAccountKey is a byte array type that can hold a literal json structure.
+// It validates that its value is valid json upon parsing. After parsing, the contents of the byte
+// array will be the original json text.
+type LiteralServiceAccountKey []byte
+
+func (k *LiteralServiceAccountKey) UnmarshalJSON(data []byte) error {
+	if k == nil {
+		return errors.New("UnmarshalJSON on nil pointer")
+	}
+
+	// First we try to parse the data as yaml/json
+	tmp := make(map[string]interface{})
+	err := json.Unmarshal(data, &tmp)
+	if err == nil {
+		*k = append((*k)[0:0], data...)
+		return nil
+	}
+
+	return errors.New("value is not valid json")
+}
+
+// EncodedServiceAccountKey is a byte array type that can hold a base64-encoded json structure.
+// It validates that its value is valid base64-encoded json upon parsing. Upon parsing, the contents
+// of the byte array will be the json text after base64 decoding is performed.
+type EncodedServiceAccountKey []byte
+
+// UnmarshalJSON sets *m to a copy of data.
+func (k *EncodedServiceAccountKey) UnmarshalJSON(data []byte) error {
+	if k == nil {
+		return errors.New("EncodedServiceAccountKey.UnmarshalJSON: nil pointer")
+	}
+
+	var decoded []byte
+	var encodedStr string
+
+	// First we decode the data into a string to get rid of any start and end quotes.
+	err := yaml.Unmarshal(data, &encodedStr)
+	if err != nil {
+		return errors.New("EncodedServiceAccountKey.UnmarshalJSON: not a string value")
+	}
+
+	decoded, err = base64.StdEncoding.DecodeString(encodedStr)
+	if err != nil {
+		return errors.New("EncodedServiceAccountKey.UnmarshalJSON: not a valid base64 value")
+	}
+
+	// Make sure what we just decoded is valid json
+	tmp := make(map[string]interface{})
+	err = json.Unmarshal(decoded, &tmp)
+	if err != nil {
+		return errors.New("EncodedServiceAccountKey.UnmarshalJSON: decoded value is not valid json")
+	}
+
+	*k = append((*k)[0:0], decoded...)
+	return nil
+}
 
 // Config contains configuration for the agent.
 type Config struct {
@@ -39,8 +97,10 @@ type Config struct {
 }
 
 // Identity contains configuration pertaining to the agent identity and credentials.
+// Exactly one of the ServiceAccountKey fields must be specified.
 type Identity struct {
-	ServiceAccountKey json.RawMessage `json:"serviceAccountKey"`
+	ServiceAccountKey        LiteralServiceAccountKey `json:"serviceAccountKey"`
+	EncodedServiceAccountKey EncodedServiceAccountKey `json:"encodedServiceAccountKey"`
 }
 
 // Metrics contains the metric definitions that the agent expects to receive.
@@ -127,6 +187,9 @@ func (c *Config) Validate() error {
 	if c.Metrics == nil {
 		return errors.New("missing metrics section")
 	}
+	if err := c.Identity.Validate(); err != nil {
+		return err
+	}
 	if err := c.Metrics.Validate(); err != nil {
 		return err
 	}
@@ -147,9 +210,33 @@ func (c *Config) Validate() error {
 }
 
 func (c *Identity) Validate() error {
-	if len(c.ServiceAccountKey) == 0 {
+	count := 0
+	if len(c.ServiceAccountKey) > 0 {
+		count += 1
+	}
+	if len(c.EncodedServiceAccountKey) > 0 {
+		count += 1
+	}
+
+	if count == 0 {
 		return errors.New("identity: missing service account key")
 	}
+	if count > 1 {
+		return errors.New("identity: too many service account keys")
+	}
+
+	return nil
+}
+
+func (c *Identity) GetServiceAccountKey() []byte {
+	if len(c.ServiceAccountKey) > 0 {
+		return c.ServiceAccountKey
+	}
+
+	if len(c.EncodedServiceAccountKey) > 0 {
+		return c.EncodedServiceAccountKey
+	}
+
 	return nil
 }
 
