1831d317dcab8819c8ed19319fde5954163c67a2
Jeremy Volkman <volkman@google.com>
2018-03-16 15:35:14 -0700

    Adds tests for two separate agents

--- sdk/python2/test.py
+++ sdk/python2/test.py
@@ -72,41 +72,59 @@ def report_now(name, value):
 class AgentSdkTest(unittest.TestCase):
   def setUp(self):
     self.tempDir = tempfile.mkdtemp('ubbagent-test')
-    self.reportDir = path.join(self.tempDir, 'reports')
-    self.stateDir = path.join(self.tempDir, 'state')
-    config = config_template.format(reportDir=self.reportDir)
-    self.agent = ubbagent.Agent(config, self.stateDir)
+    self.reportDir1 = path.join(self.tempDir, 'agent1', 'reports')
+    self.stateDir1 = path.join(self.tempDir, 'agent1', 'state')
+    self.reportDir2 = path.join(self.tempDir, 'agent2', 'reports')
+    self.stateDir2 = path.join(self.tempDir, 'agent2', 'state')
+    config1 = config_template.format(reportDir=self.reportDir1)
+    config2 = config_template.format(reportDir=self.reportDir2)
+    self.agent1 = ubbagent.Agent(config1, self.stateDir1)
+    self.agent2 = ubbagent.Agent(config2, self.stateDir2)
 
   def tearDown(self):
-    self.agent.shutdown()
-    self.agent = None
+    self.agent1.shutdown()
+    self.agent1 = None
+    self.agent2.shutdown()
+    self.agent2 = None
     shutil.rmtree(self.tempDir)
 
   def testAgent(self):
     # Add some request reports
     for _ in range(10):
-      self.agent.add_report(report_now('requests', 10))
+      self.agent1.add_report(report_now('requests', 10))   # 10*10 = 100 reqs
+      self.agent2.add_report(report_now('requests', 100))  # 10*100 = 1000 reqs
 
     # Sleep to give the heartbeat time to send reports.
     time.sleep(2)
 
     # Now check output to make sure we see expected metric reports
-    found_requests = False
-    found_instance_time = False
-    for report_file in os.listdir(self.reportDir):
-      with open(path.join(self.reportDir, report_file)) as f:
+    found_requests1 = False
+    found_requests2 = False
+    found_instance_time1 = False
+    for report_file in os.listdir(self.reportDir1):
+      with open(path.join(self.reportDir1, report_file)) as f:
         report = json.load(f)
       if report['name'] == 'requests' and report['value']['int64Value'] == 100:
-        found_requests = True
+        found_requests1 = True
       elif report['name'] == 'instance_time' and report[
         'value']['int64Value'] == 1:
-        found_instance_time = True
+        found_instance_time1 = True
+
+    for report_file in os.listdir(self.reportDir2):
+      with open(path.join(self.reportDir2, report_file)) as f:
+        report = json.load(f)
+      if report['name'] == 'requests' and report['value']['int64Value'] == 1000:
+        found_requests2 = True
 
     self.assertTrue(
-        found_requests, 'Did not find a "requests" report with value 100')
+        found_requests1,
+        'Did not find a "requests" report for agent 1 with value 100')
     self.assertTrue(
-        found_instance_time,
-        'Did not find an "instance_time" report with value 1')
+        found_instance_time1,
+        'Did not find an "instance_time" report for agent 1 with value 1')
+    self.assertTrue(
+        found_requests2,
+        'Did not find a "requests" report for agent 2 with value 1000')
 
 
 if __name__ == '__main__':
